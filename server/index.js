5bf78995a9d134b7d390277d305761199d6b1ac9
Myron R Schippers Jr <dubtwee@gmail.com>
2023-04-14 15:13:36 -0500

    organizing a new folder structure for the heroku/node deployment static site

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
-// app.use('/', express.static('public'));
+app.use('/', express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
